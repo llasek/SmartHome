ccca5786aae70b8e26f6f6b1bb343a21812f51f9
Lukasz Lasek <lukasz.lasek@gmail.com>
2021-11-03 22:57:57 +0100

    Added an alternate WiFi AP.
    Added means to configure 2 WiFi APs in wifi_cfg file.
    Cycle the configured APs on wifi disconnect.
    
    Signed-off-by: Lukasz Lasek <lukasz.lasek@gmail.com>

--- LightSwitch/src/WiFiHelper.cpp
+++ LightSwitch/src/WiFiHelper.cpp
@@ -6,24 +6,43 @@
 #include "WiFiHelper.h"
 #include "Utils.h"
 
+CWiFiHelper::CWiFiHelper()
+    : m_nCurAP( 0 )
+{
+}
+
 void CWiFiHelper::ReadCfg()
 {
     File file = LittleFS.open( FS_WIFI_CFG, "r" );
     if( file )
     {
-        m_strSsid = CfgFileReadLine( file );
-        m_strPwd = CfgFileReadLine( file );
-        m_nConnTimeout = CfgFileReadLine( file ).toInt();
         m_strHostname = CfgFileReadLine( file );
+        m_nConnTimeout = CfgFileReadLine( file ).toInt();
+        for( int nIdx = 0; nIdx <= m_nCurAP; nIdx++ )
+        {
+            CfgFileReadLine( file );
+            m_strSsid = CfgFileReadLine( file );
+            m_strPwd = CfgFileReadLine( file );
+        }
         file.close();
 
-        DBGLOG4( "wifi cfg: ssid:'%s' pwd:'%s' timeo:%lus hostname:'%s'\n",
-            m_strSsid.c_str(), m_strPwd.c_str(), m_nConnTimeout, m_strHostname.c_str());
+        DBGLOG5( "wifi cfg %d: ssid:'%s' pwd:'%s' timeo:%lus hostname:'%s'\n",
+            m_nCurAP, m_strSsid.c_str(), m_strPwd.c_str(), m_nConnTimeout, m_strHostname.c_str());
         m_nConnTimeout *= 1000;
     }
     else
     {
         DBGLOG( "wifi cfg missing" );
+    }
+}
+
+void CWiFiHelper::AlternateCfg()
+{
+    if( LittleFS.begin())
+    {
+        m_nCurAP = 1 - m_nCurAP;
+        ReadCfg();
+        LittleFS.end();
     }
 }
 
@@ -34,8 +53,8 @@ String& CWiFiHelper::GetHostName()
 
 void CWiFiHelper::Enable()
 {
-    Init( m_strSsid.c_str(), m_strPwd.c_str(), m_nConnTimeout );
-    SetupSta( m_strHostname.c_str());
+    Init( m_nConnTimeout );
+    SetupSta( m_strSsid.c_str(), m_strPwd.c_str(), m_strHostname.c_str());
 }
 
 void CWiFiHelper::OnConnect()
@@ -47,7 +66,8 @@ void CWiFiHelper::OnConnect()
 
 void CWiFiHelper::OnDisconnect()
 {
-    SetupSta( m_strHostname.c_str());
+    AlternateCfg();
+    SetupSta( m_strSsid.c_str(), m_strPwd.c_str(), m_strHostname.c_str());
 }
 
 void CWiFiHelper::loop()
